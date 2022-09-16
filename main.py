3b33d72e626dfb4b3999c0f68aba999c5f76b944
eerts <113695053+eerts@users.noreply.github.com>
2022-09-16 15:00:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from tridy.Evidence import Evidence
+
+#zobrazí menu s volbou
+def main():
+    evidence = Evidence()
+    while True:
+        choice = evidence.menu()
+        if choice == 0:
+            print("\n\nUkončuji...")
+            break
+        elif choice == 1:
+            evidence.addPojistenec()
+        elif choice == 2:
+            evidence.printEvidence()
+        else:
+            evidence.search()
+
+#vypíše úvod 
+if __name__ == '__main__':
+    print("----------------------------")
+    print("Evidence pojištěných")
+    print("----------------------------")
+    main()
+
