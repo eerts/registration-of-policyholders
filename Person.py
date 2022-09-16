92f27221f04bf2aaabbc2e09a7075ef499d74672
Daniel <113695053+eerts@users.noreply.github.com>
2022-09-16 15:20:18 +0200

    Update Person.py

--- Person.py
+++ Person.py
@@ -6,6 +6,7 @@ class Person:
         self.surname = surname
         self.age = age
         self.number = number
+        
     #vráti - vypíse osobu
     def __str__(self):
         return "{firstname}\t{surname}\t{age}\t{number}".format(firstname=self.firstname, surname=self.surname, age=self.age, number=self.number)
